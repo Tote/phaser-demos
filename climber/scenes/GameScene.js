7ec9782eb83e424d3dd9200ad0eb53f484860699
Patricio Morel <totenbot@outlook.com>
2024-04-11 04:02:46 +0000

    Refactor: Platform generation

--- climber/scenes/GameScene.js
+++ climber/scenes/GameScene.js
@@ -16,28 +16,21 @@ class GameScene extends Phaser.Scene {
 
         //Platforms
         this.platforms = this.physics.add.staticGroup()
-        this.platforms.create(312, 800, 'grass').setScale(13, 1).refreshBody()
-        this.platforms.create(312, 700, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(0, 600, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(312, 500, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(600, 400, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(600, 300, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(400, 200, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(200, 100, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(300, 0, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(500, -100, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(600, -200, 'grass').setScale(4, 1).refreshBody()
-        this.platforms.create(200, -300, 'grass').setScale(4, 1).refreshBody()
-        this.lastPlatformHeigth = -300
-        
+        this.platforms.create(300, 800, 'grass').setScale(13, 1).refreshBody()
+        this.lastPlatformHeigth = 800
+
+        while (this.lastPlatformHeigth > 0) {
+            this.spawnPlatform()
+        }
+
 
         //Player
         this.player = this.physics.add.sprite(100, 700, 'ball', 0)
         this.player.setScale(2)
         this.player.setDepth(1)
-        this.player.body.checkCollision.left   = false
-        this.player.body.checkCollision.right  = false
-        this.player.body.checkCollision.up     = false
+        this.player.body.checkCollision.left = false
+        this.player.body.checkCollision.right = false
+        this.player.body.checkCollision.up = false
         this.anims.create({
             key: 'ball-jump',
             frames: this.anims.generateFrameNumbers('ball', { start: 8, end: 13 }),
@@ -59,25 +52,21 @@ class GameScene extends Phaser.Scene {
         //Controls
         this.controls = this.input.keyboard.addKeys('W,A,S,D,SPACE,LEFT,RIGHT,UP,DOWN')
         this.cameras.main.setBounds(0, -10000, 600, 10800)
-        //this.cameras.main.startFollow(this.player)
-        //this.cameras.main.setDeadzone(600,400)
 
     }
 
     update() {
         // Player movement
-        //if(this.player.body.onFloor()){
-            if (this.controls.LEFT.isDown || this.controls.A.isDown) {
-                this.player.setVelocityX(-250)
-                this.player.setFlipX(true)
+        if (this.controls.LEFT.isDown || this.controls.A.isDown) {
+            this.player.setVelocityX(-300)
+            this.player.setFlipX(true)
 
-            } else if (this.controls.RIGHT.isDown || this.controls.D.isDown) {
-                this.player.setVelocityX(250)
-                this.player.setFlipX(false)
-            } else {
-                this.player.setVelocityX(0)
-            }
-        //}
+        } else if (this.controls.RIGHT.isDown || this.controls.D.isDown) {
+            this.player.setVelocityX(300)
+            this.player.setFlipX(false)
+        } else {
+            this.player.setVelocityX(0)
+        }
 
         if (this.controls.SPACE.isDown && this.player.body.touching.down) {
             this.player.setVelocityY(-270)
@@ -88,19 +77,29 @@ class GameScene extends Phaser.Scene {
             this.player.setVelocityX(0)
             this.player.setVelocityY(750)
         }
-        if(!this.player.anims.isPlaying && this.player.isFalling && this.player.body.onFloor()){
+        if (!this.player.anims.isPlaying && this.player.isFalling && this.player.body.onFloor()) {
             this.player.anims.play('ball-land')
             this.player.isFalling = false
         }
 
         //Camera
-        if(this.player.y < this.cameras.main.scrollY + this.cameras.main.height / 2){
+        if (this.player.y < this.cameras.main.scrollY + this.cameras.main.height / 2) {
             this.cameras.main.scrollY = this.player.y - this.cameras.main.height / 2
         }
-        if(this.cameras.main.scrollY < this.lastPlatformHeigth){
-            this.platforms.create(312, this.lastPlatformHeigth-100, 'grass')
-            this.lastPlatformHeigth -= 100
+        if (this.cameras.main.scrollY < this.lastPlatformHeigth) {
+            this.spawnPlatform()
         }
 
+    }
+
+    spawnPlatform(y) {
+        const _x = Phaser.Math.Between(0, 600)
+        const _y = y || this.lastPlatformHeigth - 100
+
+        this.platforms
+            .create(_x, _y, 'grass')
+            .setScale(4, 1)
+            .refreshBody()
+        this.lastPlatformHeigth = _y
     }
 }
